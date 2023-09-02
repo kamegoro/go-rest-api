9d0cd3282ec7c50d1b76f5cb41af5a853dde28fa
kamegoro <tentyo0620@icloud.com>
2023-09-02 19:54:11 +0900

    add router

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type todo struct {
 	ID        string `json:"id"`
 	Item      string `json:"title"`
@@ -22,4 +28,14 @@ var todos = []todo{
 		Item:      "Record Video",
 		Completed: false,
 	},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:9090")
 }
