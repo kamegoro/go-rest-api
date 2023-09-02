a14cd2e732c4b18d18fcfb0ca1ffd4e516658a48
kamegoro <tentyo0620@icloud.com>
2023-09-02 19:48:12 +0900

    go fmt

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 type todo struct {
-	ID	string
-	Item string
-	Completed bool
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
 var todos = []todo{
 	{
-		ID: "1",
-		Item: "Clean Room",
+		ID:        "1",
+		Item:      "Clean Room",
 		Completed: false,
 	},
 	{
-		ID: "2",
-		Item: "Read Book",
+		ID:        "2",
+		Item:      "Read Book",
 		Completed: false,
 	},
 	{
-		ID: "3",
-		Item: "Record Video",
+		ID:        "3",
+		Item:      "Record Video",
 		Completed: false,
 	},
 }
