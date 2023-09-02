9eeb61526d23ff7c4cc8b08b6ed25f1e2ebc22a6
kamegoro <tentyo0620@icloud.com>
2023-09-02 20:16:57 +0900

    add patch req

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func getTodo(context *gin.Context) {
 
 	if err != nil {
 		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
 	}
 
 	context.IndentedJSON(http.StatusOK, todo)
@@ -69,10 +70,26 @@ func getTodoById(id string) (*todo, error) {
 	return nil, errors.New("todo not found")
 }
 
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
 	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
